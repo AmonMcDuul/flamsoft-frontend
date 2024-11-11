0026ce662c22a49cbb443da96f16a2f194698c3c
Joran Schols <schols.joran@gmail.com>
2024-11-11 20:32:15 +0100

    init site

---
+++ src/app/pages/projecten/projecten.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projecten',
+  standalone: true,
+  imports: [],
+  templateUrl: './projecten.component.html',
+  styleUrl: './projecten.component.scss'
+})
+export class ProjectenComponent {
+
+}
