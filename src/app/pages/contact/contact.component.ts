3b7e06ef7071a53ed4af68f9cd7b496d6b1006dc
Joran Schols <schols.joran@gmail.com>
2024-11-18 20:53:38 +0100

    contact wip

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
-
-}
+    contactForm!: FormGroup;
+    showContactFields = false;
+    showConfirmation = false;
+  
+    ngOnInit(): void {
+      // Initialize the form group with controls
+      this.contactForm = new FormGroup({
+        idea: new FormControl('', [Validators.required]),
+        name: new FormControl('', 
+            // [Validators.required]
+        ),
+        email: new FormControl('', [Validators.required, Validators.email]),
+        phone: new FormControl('', [
+        //   Validators.required,
+        //   Validators.pattern(/^\+?\d{10,}$/)
+        ])
+      });
+  
+      this.contactForm.get('idea')?.valueChanges.subscribe(value => {
+        this.showContactFields = value.trim().length > 0;
+      });
+    }
+  
+    onSubmit(): void {
+      if (this.contactForm.valid) {
+        console.log('Form Data:', this.contactForm.value);
+        this.resetForm();
+        this.showConfirmation = true;
+      }
+    }
+  
+    resetForm(): void {
+      this.contactForm.reset();
+      this.showContactFields = false;
+    }
+  }
