b058c0630aa220e81ae2dc4ac155795a133d6ca2
Joran Schols <schols.joran@gmail.com>
2025-02-25 23:38:53 +0100

    responsive

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -5,8 +5,13 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  isMenuOpen = false;
 
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+    console.log('Menu toggled:', this.isMenuOpen); 
+  }
 }
