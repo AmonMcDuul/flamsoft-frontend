48422f473d60e55ea32f1058f155acf338e2be61
Joran Schols <schols.joran@gmail.com>
2025-02-26 21:03:54 +0100

    email service

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { EmailService } from '../../services/email.service';
 
 @Component({
   selector: 'app-contact',
@@ -13,19 +14,17 @@ export class ContactComponent {
     contactForm!: FormGroup;
     showContactFields = false;
     showConfirmation = false;
-  
+
+    constructor(private emailService: EmailService) {}
+
     ngOnInit(): void {
       // Initialize the form group with controls
       this.contactForm = new FormGroup({
         idea: new FormControl('', [Validators.required]),
-        name: new FormControl('', 
-            // [Validators.required]
-        ),
+        name: new FormControl(''),
         email: new FormControl('', [Validators.required, Validators.email]),
-        phone: new FormControl('', [
-        //   Validators.required,
-        //   Validators.pattern(/^\+?\d{10,}$/)
-        ])
+        phone: new FormControl(''),
+        honeypot: new FormControl('')
       });
   
       this.contactForm.get('idea')?.valueChanges.subscribe(value => {
@@ -34,14 +33,28 @@ export class ContactComponent {
     }
   
     onSubmit(): void {
+      if (this.contactForm.get('honeypot')?.value !== '')  {
+        return;
+      }
       if (this.contactForm.valid) {
-        console.log('Form Data:', this.contactForm.value);
-        this.resetForm();
-        this.showConfirmation = true;
+
+        const subject = `Flamsoft Contact Form Submission from ${this.contactForm.get('name')?.value}`;
+        const body = `Name: ${this.contactForm.get('name')?.value}\nEmail: ${this.contactForm.get('email')?.value}\nTelephone: ${this.contactForm.get('phone')?.value}\n\nMessage:\n${this.contactForm.get('idea')?.value}`;
     
-        setTimeout(() => {
-          this.showConfirmation = false;
-        }, 10000); 
+        this.emailService.sendEmail(subject, body).subscribe(
+          response => {
+            console.log('Email sent successfully', response);
+            this.resetForm();
+            this.showConfirmation = true;
+        
+            setTimeout(() => {
+              this.showConfirmation = false;
+            }, 10000); 
+          },
+          error => {
+            console.error('Error sending email', error);
+          }
+        );
       }
     }
   
